4cfac522eba2f16a8016f1b088c25c744b06279c
James Coglan <jcoglan@gmail.com>
2017-05-31 13:28:50 +0100

    Implement TAPL 11-7, records.

---
+++ test/records.rb
@@ -0,0 +1,27 @@
+require_relative './_typeof'
+
+LANG = Infer.lang('./tapl/11-7-records.txt')
+EVAL = LANG.relation('->')
+
+def many(expr)
+  expr = Infer.expr(expr)
+
+  loop do
+    puts expr
+    begin
+      expr = EVAL.once(expr)
+    rescue Infer::Stuck
+      break
+    end
+  end
+  puts
+end
+
+many 'if true then true else false'
+many 'hello: true, ⊥'
+many 'hello: (pred (succ 0)), ⊥'
+many 'hello: (pred (succ 0)), (world: (iszero 0), ⊥)'
+many '(hello: (pred (succ 0)), (world: (iszero 0), ⊥)).hello'
+
+typeof LANG, 'hello: (pred (succ 0)), (world: (iszero 0), ⊥)'
+typeof LANG, '(hello: (pred (succ 0)), (world: (iszero 0), ⊥)).world'
