072902e2e2b4af1b98a80a9fc45d223a9c3effdd
James Coglan <jcoglan@gmail.com>
2017-05-31 13:28:14 +0100

    Implement TAPL 11-5, pairs.

---
+++ test/pairs.rb
@@ -0,0 +1,9 @@
+require_relative './_typeof'
+
+lang = Infer.lang('./tapl/11-5-pairs.txt')
+
+typeof lang, 'λx : Bool. x'
+typeof lang, 'λx : Bool. (x,x)'
+typeof lang, '(λx : Bool. (x,x)) true'
+typeof lang, '((λx : Bool. (x,x)) true).2'
+typeof lang, '(((λx : Bool. (λy : Bool. (x,y))) true) false).2'
