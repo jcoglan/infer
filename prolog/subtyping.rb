2742128a13f19fd039ff81207e9f200b06213548
James Coglan <jcoglan@gmail.com>
2017-09-06 23:13:42 +0100

    Add another example to the prolog subtyping test.

--- prolog/subtyping.rb
+++ prolog/subtyping.rb
@@ -3,9 +3,14 @@ require_relative '../lib/infer'
 source  = File.read(File.expand_path('../subtyping.pl', __FILE__))
 program = Infer::Prolog.program(source)
 
-query, vars = Infer::Prolog.query <<-Q
-  type([], app(λ(f, arrow(rcd([[x, nat]]), rcd([])), app(f, rec([[x, 0]]))), λ(r, rcd([]), rec([[y, true]]))), T).
-Q
+exprs = [
+  'type([], λ(x, bool, λ(y, bool, λ(x, nat, x))), T).',
+  'type([], app(λ(f, arrow(rcd([[x, nat]]), rcd([])), app(f, rec([[x, 0]]))), λ(r, rcd([]), rec([[y, true]]))), T).'
+]
 
-states = program.derive(query)
-Infer::Prolog.print_results(states, vars)
+exprs.each do |expr|
+  query, vars = Infer::Prolog.query(expr)
+
+  states = program.derive(query)
+  Infer::Prolog.print_results(states, vars)
+end
